6c66628c8f7aa884a26f5a1740c13ed9f48bd8e6
Péter Bócz <boczpeter+laptop@gmail.com>
2021-12-01 10:51:56 +0100

    install+config unplugin-icons

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
+import Icons from 'unplugin-icons/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		target: 'body'
+		target: 'body',
+		vite: {
+      plugins: [
+        Icons({
+          compiler: 'svelte',
+        }),
+      ],
+    },
 	}
 };
 
