12563735788b7bcf2e6b0df3e7c30d5e6db1dc54
Péter Bócz <boczpeter@gmail.com>
2022-01-10 00:56:22 +0100

    static adapter for cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-// import Icons from 'unplugin-icons/vite'
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
