e317acabd8c223c9aa5276b7081706ad307819b5
Péter Bócz <boczpeter@gmail.com>
2022-01-11 02:55:38 +0100

    migrate from svelte-markdown to mdsvex

--- src/routes/help.svelte
+++ src/routes/help.svelte
@@ -1,10 +1,29 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
 	import Popup from '$lib/Popup.svelte';
-	import Help  from '$lib/Help.svelte';
+	import Help  from '$lib/Help.md';
+	import { externalLink } from '$lib/stores.js';
+  let out;
+  $: externalLink(out, 'a');  // postprocess all links in markdown
 </script>
 
-<Popup><Help/></Popup>
+<Popup>
+	<output bind:this={out}>
+		<Help/>
+		<img src="mklogo.png" alt="MK logo">
+	</output>
+</Popup>
+
+<style>
+  img { /* MK logo */
+    margin: auto;
+    display: block;
+    min-width: calc(10 * var(--font));
+  }
+	output {
+		color: #000;
+		font-size: var(--font);
+		overflow-y: auto;
+    overscroll-behavior: none;
+    grid-area: popup-content;
+	}
+</style>
