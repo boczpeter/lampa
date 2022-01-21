a41b6160c6fba0c863fe2f5310ddc4b68f86fef4
Péter Bócz <boczpeter@gmail.com>
2022-01-21 17:52:24 +0100

    error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,10 @@
+<h3 class=error>Ilyen oldal nem található!</h3>
+
+<style>
+	h3 {
+		align-self: center;
+	}
+	:global(.error + form > h1 ~ *) { /* hide all but h1 */
+		display: none;
+	}
+</style>
