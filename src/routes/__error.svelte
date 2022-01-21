f43346b23633aac32138150bf3f610165844fc5f
Péter Bócz <boczpeter@gmail.com>
2022-01-21 18:42:28 +0100

    button for error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,10 +1,34 @@
-<h3 class=error>Ilyen oldal nem található!</h3>
+<script>
+	import Icon from '$lib/Icon.svelte';
+</script>
+
+<div class=error>
+	<h2>Ilyen oldal nem található!</h2>
+	<a href="/" role=button><Icon icon=arrow-left>Vissza a fő képernyőre</Icon></a>
+</div>
+
 
 <style>
-	h3 {
-		align-self: center;
+	div {
+		order: 2;
+		display: flex;
+		flex-direction: column;
+		gap: 3em;
+		place-items: center;
 	}
-	:global(.error + form > h1 ~ *) { /* hide all but h1 */
+	h2 {
+		color: var(--red);
+	}
+	a {
+		font-size: 1.5em;
+  	background: #ccc;
+		color: #333 !important;
+		padding: var(--gap);
+		margin: var(--gap);
+		border-radius: 5em;
+		text-align: center;
+	}
+	:global(.error ~ form > h1 ~ *) { /* hide all but h1 */
 		display: none;
 	}
 </style>
