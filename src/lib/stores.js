2c671c68a07f457d2dbe5dd228680444d8742014
Péter Bócz <boczpeter@gmail.com>
2022-11-06 17:59:36 +0100

    async/await

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -6,9 +6,7 @@ export const
   latlng    = writable(null),
 
   // copy text to clipboard
-  copy = text => navigator.clipboard?.writeText(text)
-			.then(() => console.info(text))
-			.catch(r => console.error('clipboard write failed: '+r)),
+  copy = async text => await navigator.clipboard?.writeText(text)
 
   // Make node's selected links external to prevent Router from handling them
   externalLink = (node, sel) => node && node.querySelectorAll(sel).forEach(a => Object.assign(a, {rel:'external', target:'_blank'}));
