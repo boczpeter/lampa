fe2f113722903b57f7bf213cf40d38354b726626
Péter Bócz <boczpeter@gmail.com>
2023-08-24 12:01:36 +0200

    grid.js

--- src/routes/export/+page.svelte
+++ src/routes/export/+page.svelte
@@ -4,26 +4,27 @@
 	import { apiURL } from '$lib/stores'
   import { onMount } from 'svelte'
 	import PocketBase from 'pocketbase'
+	import Grid from "gridjs-svelte"
 
-	let loggedin = false, list = []
+	let loggedin = false, data = null
 
   const pb = new PocketBase(apiURL),	// admin instance
-		cols = 'user city population location gps both front back none total updated'.split(' ')
+		columns = 'user city population location gps both front back none total updated'.split(' ')
 
   onMount(() => pb.collection('users').authWithPassword('export', '123123123')
 		.then(authData => pb.collection('posts').getFullList({sort: '-created'}))
-		.then(data => list = data.map(rec => cols.map(col => rec[col])))
+		.then(rows => data = rows)
+		// .then(rows => console.info(cols.join(', ')))
 		// .finally(() => console.info(pb.authStore))
   )
-
 </script>
 
-<Popup type=help>
-	{JSON.stringify(cols)}<br>
-	{#each list as post}
-		{JSON.stringify(post)}<br>
-	{/each}
+{#if data}
+<Popup type=plain>
+		<Grid {data} {columns} />
 </Popup>
+{/if}
 
-<style>
+<style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
 </style>
