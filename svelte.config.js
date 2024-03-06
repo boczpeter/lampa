9ec9f9c62e7c31d879204326f119d1bc52fa3e5e
Péter Bócz <peter@bocz.org>
2024-03-06 20:11:56 +0100

    sk upgrade

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      precompress: true // generate .gz and .br files
+    }),
 	},
 
-  preprocess: [mdsvex(mdsvexConfig)],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 };
 
 export default config;
